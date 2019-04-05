ece97c65deba2149454218a8e77a7ea233130b0f
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-04-05 00:27:03 -0300

    41. Styling with Buttons and Sliders

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Carlos Eduardo on 04/04/19.
+//  Copyright © 2019 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
